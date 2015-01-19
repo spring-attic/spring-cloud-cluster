107f8987f4943fc408a0546cef24bbea25d62bbd
Patrick Peralta <pperalta@pivotal.io>
2015-01-19 07:41:12 -0500

    Tests use CountDownLatch instead of sleep

--- spring-cloud-cluster-hazelcast/src/test/java/org/springframework/cloud/cluster/hazelcast/leader/HazelcastTests.java
+++ spring-cloud-cluster-hazelcast/src/test/java/org/springframework/cloud/cluster/hazelcast/leader/HazelcastTests.java
@@ -18,22 +18,25 @@ package org.springframework.cloud.cluster.hazelcast.leader;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 import org.junit.Test;
+
 import org.springframework.cloud.cluster.leader.Context;
 import org.springframework.cloud.cluster.leader.DefaultCandidate;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-
 /**
  * Tests for hazelcast leader election.
- * 
+ *
  * @author Janne Valkealahti
+ * @author Patrick Peralta
  *
  */
 public class HazelcastTests {
@@ -43,41 +46,40 @@ public class HazelcastTests {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
 				Config1.class);
 		TestCandidate candidate = ctx.getBean(TestCandidate.class);
-		Thread.sleep(2000);
-		assertThat(candidate.onGrantedCalled.get(), is(true));
+		assertThat(candidate.onGrantedLatch.await(5, TimeUnit.SECONDS), is(true));
 		ctx.close();
 	}
-	
+
 	@Configuration
 	static class Config1 {
-		
+
 		@Bean
 		public TestCandidate candidate() {
 			return new TestCandidate();
 		}
-		
-		@Bean
-        public HazelcastInstance hazelcastInstance() {
-                return Hazelcast.newHazelcastInstance();
-        }
 
-        @Bean
-        public LeaderInitiator initiator() {
-                return new LeaderInitiator(hazelcastInstance(), candidate());
-        }
-		
+		@Bean
+		public HazelcastInstance hazelcastInstance() {
+			return Hazelcast.newHazelcastInstance();
+		}
+
+		@Bean
+		public LeaderInitiator initiator() {
+			return new LeaderInitiator(hazelcastInstance(), candidate());
+		}
+
 	}
 
 	static class TestCandidate extends DefaultCandidate {
-		
-		AtomicBoolean onGrantedCalled = new AtomicBoolean(false);
-		
+
+		CountDownLatch onGrantedLatch = new CountDownLatch(1);
+
 		@Override
 		public void onGranted(Context ctx) {
-			onGrantedCalled.set(true);
+			onGrantedLatch.countDown();
 			super.onGranted(ctx);
 		}
-		
+
 	}
-	
+
 }
--- spring-cloud-cluster-zookeeper/src/test/java/org/springframework/cloud/cluster/zk/leader/ZookeeperTests.java
+++ spring-cloud-cluster-zookeeper/src/test/java/org/springframework/cloud/cluster/zk/leader/ZookeeperTests.java
@@ -19,13 +19,16 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.test.TestingServer;
+
 import org.junit.Test;
+
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.cluster.leader.Context;
@@ -36,8 +39,9 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * Tests for zookeeper leader election.
- * 
+ *
  * @author Janne Valkealahti
+ * @author Patrick Peralta
  *
  */
 public class ZookeeperTests {
@@ -47,27 +51,26 @@ public class ZookeeperTests {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
 				ZkServerConfig.class, Config1.class);
 		TestCandidate candidate = ctx.getBean(TestCandidate.class);
-		Thread.sleep(2000);
-		assertThat(candidate.onGrantedCalled.get(), is(true));
+		assertThat(candidate.onGrantedLatch.await(5, TimeUnit.SECONDS), is(true));
 		ctx.close();
 	}
 
 	@Configuration
 	static class ZkServerConfig {
-		
+
 		@Bean
 		public TestingServerWrapper testingServerWrapper() throws Exception {
 			return new TestingServerWrapper();
 		}
-		
+
 	}
-	
+
 	@Configuration
 	static class Config1 {
 
 		@Autowired
 		TestingServerWrapper testingServerWrapper;
-		
+
 		@Bean
 		public TestCandidate candidate() {
 			return new TestCandidate();
@@ -88,11 +91,11 @@ public class ZookeeperTests {
 		}
 
 	}
-	
+
 	static class TestingServerWrapper implements DisposableBean {
-		
+
 		TestingServer testingServer;
-		
+
 		public TestingServerWrapper() throws Exception {
 			this.testingServer = new TestingServer(true);
 		}
@@ -101,26 +104,27 @@ public class ZookeeperTests {
 		public void destroy() throws Exception {
 			try {
 				testingServer.close();
-			} catch (IOException e) {
+			}
+			catch (IOException e) {
 			}
 		}
-		
+
 		public int getPort() {
 			return testingServer.getPort();
 		}
-		
+
 	}
-	
+
 	static class TestCandidate extends DefaultCandidate {
-		
-		AtomicBoolean onGrantedCalled = new AtomicBoolean(false);
-		
+
+		CountDownLatch onGrantedLatch = new CountDownLatch(1);
+
 		@Override
 		public void onGranted(Context ctx) {
-			onGrantedCalled.set(true);
+			onGrantedLatch.countDown();
 			super.onGranted(ctx);
 		}
-		
+
 	}
-	
+
 }
